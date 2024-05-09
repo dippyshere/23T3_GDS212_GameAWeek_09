1d076367bc8b749bef09d1870ce7d4afb19df4a3
Alex Hanson <43489279+dippyshere@users.noreply.github.com>
2024-05-10 02:05:51 +1000

    upgrade to unity 6 + new input system + minor optimisations

--- Assets/Scripts/DebugPlayerTeleporter.cs
+++ Assets/Scripts/DebugPlayerTeleporter.cs
@@ -2,61 +2,66 @@ using StarterAssets;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class DebugPlayerTeleporter : MonoBehaviour
 {
     public GameObject playerObject;
     public List<Transform> locations = new List<Transform>();
 
-    // Start is called before the first frame update
-    void Start()
+    private void OnEnable()
     {
-        
+        Keyboard.current.onTextInput += OnTextInput;
     }
 
-    // Update is called once per frame
-    void LateUpdate()
+    private void OnDisable()
+    {
+        Keyboard.current.onTextInput -= OnTextInput;
+    }
+
+    private void OnTextInput(char character)
     {
         playerObject.GetComponent<ThirdPersonController>()._controller.enabled = false;
-        switch (Input.inputString)
+        switch (character)
         {
-            case "1":
+            case '1':
+
                 playerObject.transform.localPosition = locations[0].position;
                 playerObject.transform.rotation = locations[0].rotation;
                 break;
-            case "2":
+            case '2':
                 playerObject.transform.localPosition = locations[1].position;
                 playerObject.transform.rotation = locations[1].rotation;
                 break;
-            case "3":
+            case '3':
                 playerObject.transform.localPosition = locations[2].position;
                 playerObject.transform.rotation = locations[2].rotation;
                 break;
-            case "4":
+            case '4':
                 playerObject.transform.localPosition = locations[3].position;
                 playerObject.transform.rotation = locations[3].rotation;
                 break;
-            case "5":
+            case '5':
                 playerObject.transform.localPosition = locations[4].position;
                 playerObject.transform.rotation = locations[4].rotation;
                 break;
-            case "6":
+            case '6':
                 playerObject.transform.localPosition = locations[5].position;
                 playerObject.transform.rotation = locations[5].rotation;
                 break;
-            case "7":
+            case '7':
                 playerObject.transform.localPosition = locations[6].position;
                 playerObject.transform.rotation = locations[6].rotation;
                 break;
-            case "8":
+            case '8':
                 playerObject.transform.localPosition = locations[7].position;
                 playerObject.transform.rotation = locations[7].rotation;
                 break;
-            case "9":
+            case '9':
                 playerObject.transform.localPosition = locations[8].position;
                 playerObject.transform.rotation = locations[8].rotation;
                 break;
-            case "0":
+            case '0':
                 playerObject.transform.localPosition = locations[9].position;
                 playerObject.transform.rotation = locations[9].rotation;
                 break;
@@ -68,7 +73,7 @@ public class DebugPlayerTeleporter : MonoBehaviour
 
     public void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "Player")
+        if (other.gameObject.CompareTag("Player"))
         {
             playerObject.transform.localPosition = locations[0].position;
             playerObject.transform.rotation = locations[0].rotation;
